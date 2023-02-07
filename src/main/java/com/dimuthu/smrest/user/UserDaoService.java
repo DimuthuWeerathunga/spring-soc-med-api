facbf067964d9c5d1f034be2e3630fc82764de0f
Dimuthu Weerathunga <dimu.wa.98@gmail.com>
2023-02-07 22:43:28 +0530

    userdaoservice configuration

--- src/main/java/com/dimuthu/smrest/user/UserDaoService.java
+++ src/main/java/com/dimuthu/smrest/user/UserDaoService.java
@@ -1,4 +1,24 @@
 package com.dimuthu.smrest.user;
 
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class UserDaoService {
+
+    private static List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(1, "Eve", LocalDate.now().minusYears(35)));
+        users.add(new User(1, "James", LocalDate.now().minusYears(20)));
+    }
+
+    public List<User> findAll(){
+        return users;
+    }
+
 }
