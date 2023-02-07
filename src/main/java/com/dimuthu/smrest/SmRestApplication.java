0ae187aca55a70bbb48fd503d151f24ddec34ab7
Dimuthu Weerathunga <dimu.wa.98@gmail.com>
2023-02-07 22:08:05 +0530

    initial commit

---
+++ src/main/java/com/dimuthu/smrest/SmRestApplication.java
@@ -0,0 +1,13 @@
+package com.dimuthu.smrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dimuthu/smrest/SmRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimuthu.smrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
